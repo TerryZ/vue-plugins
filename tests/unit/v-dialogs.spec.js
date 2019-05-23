50314276050de22d845dd1652d748e3318f7badf
TerryZ <terry5@foxmail.com>
2019-05-23 21:04:31 +0800

    update

--- tests/unit/v-dialogs.spec.js
+++ tests/unit/v-dialogs.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount, mount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import dialogs from '@/components/v-dialogs/Container';
 import demo from '@/views/demo/Profile';
 
