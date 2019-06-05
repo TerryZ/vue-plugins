f9b798feb665f8ce8210b8faeb1d6c83a4ee5cb8
Terry Zeng <terry5@foxmail.com>
2019-06-05 15:54:17 +0800

    update

---
+++ src/components/v-sidebar/SlideBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div v-show="container">
+        <div class="slide-bar-container">
+            <div class="slide-bar__dialog" @click.stop="outSideClick">
+                <transition name="slide-bar--fade-right">
+                <div class="slide-bar__content" :class="themeClass" v-show="show" :style="{width:dialogWidth+'px'}">
+                    <div class="slide-bar__title">
+                        <div class="slide-bar__title-content" v-text="title" v-if="title"></div>
+                        <div class="slide-bar__title-close" @click.stop="close">
+                            <i class="el-icon-close"></i>
+                        </div>
+                    </div>
+                    <div class="slide-bar__body">
+                        <slot></slot>
+                    </div>
+                </div>
+                </transition>
+            </div>
+        </div>
+        <transition name="slide-bar--fade">
+            <div class="slide-bar__overlay" v-if="backdrop" v-show="show"></div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    import './slide.scss';
+    import mixins from './mixins';
+	export default {
+		name: "SlideBar",
+        mixins: [mixins]
+	};
+</script>
---
+++ src/components/v-sidebar/mixins/index.js
@@ -0,0 +1,62 @@
+export default {
+	props: {
+		value: {
+			type: Boolean,
+			default: false
+		},
+		title: {
+			type: [String,Boolean],
+			default: 'Slide-bar'
+		},
+		width: Number,
+		theme: {
+			type: String,
+			default: ''
+		},
+		backdrop: {
+			type: Boolean,
+			default: true
+		}
+	},
+	data(){
+		return {
+			container: this.value,
+			show: this.value
+		};
+	},
+	watch: {
+		value(val){
+			if(val) {
+				this.show = val;
+				this.container = val;
+			}else this.close();
+		}
+	},
+	computed: {
+		themeClass(){
+			switch(this.theme){
+				case 'dark': return 'slide-bar--dark';
+				default: return '';
+			}
+		},
+		dialogWidth(){
+			const MIN_WIDTH = 400;
+			return this.width && this.width > MIN_WIDTH ? this.width : MIN_WIDTH;
+		}
+	},
+	methods:{
+		close(){
+			this.show = false;
+			setTimeout(()=>{
+				this.container = false;
+				this.$emit('input', false);
+			},200);
+		},
+		outSideClick(e){
+			if(!e.path.some(val=>val.className && typeof val.className === 'string' &&
+				val.className.includes('slide-bar__content'))) {
+				this.close();
+			}
+		}
+	}
+};
