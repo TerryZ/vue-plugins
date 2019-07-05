f66c37e2750f6266e65eaee668798e2a7cbfdb4c
Terry Zeng <terry5@foxmail.com>
2019-07-05 11:43:14 +0800

    udpate

--- src/components/v-selectmenu/SelectMenu.vue
+++ src/components/v-selectmenu/SelectMenu.vue
@@ -1,67 +1,65 @@
 <template>
-    <div class="v-selectmenu">
+    <!-- drop down list -->
+    <v-dropdown class="v-selectmenu" ref="drop" @show-change="showChange"
+                :align="align"
+                :embed="embed"
+                :x="x"
+                :y="y"
+                :right-click="rightClick" >
 
-        <div class="caller-container" ref="caller"
-             v-if="!embed"
-             @mouseenter="moveIn"
-             @mouseleave="moveOut"
-             @contextmenu="mouseRight"
-             @click="click">
-            <slot :show="show">
-                <button type="button" :class="['sm-default-btn', {'sm-opened': show}]">
-                    {{btnText}}
-                    <span class="sm-caret-down"></span>
-                </button>
-            </slot>
+        <template #caller v-if="!embed">
+            <div class="caller-container" ref="caller"
+                 @mouseenter="moveIn"
+                 @mouseleave="moveOut"
+                 @contextmenu="mouseRight"
+                 @click="click">
+                <slot :show="show">
+                    <button type="button" :class="['sm-default-btn', {'sm-opened': show}]">
+                        {{btnText}}
+                        <span class="sm-caret-down"></span>
+                    </button>
+                </slot>
+            </div>
+        </template>
+
+        <!-- header bar -->
+        <div class="sm-header" v-if="title || !regular || state.group">
+            <h3 v-text="headerText"></h3>
+            <button type="button" :title="i18n.select_all_btn" class="sm-selectall-button"
+                    @click="selectAll" v-if="!regular && multiple"><i class="sm-iconfont icon-selectall"></i></button>
+            <button type="button" :title="i18n.remove_all_btn" class="sm-removeall-button" @click="clear"
+                    v-if="!regular"><i class="sm-iconfont icon-removeall"></i></button>
+            <button type="button" @click="close" :title="i18n.close_btn" v-if="!embed"
+                    class="sm-close-button">×</button>
         </div>
 
-        <!-- drop down list -->
-        <v-dropdown ref="drop" @show-change="showChange"
-                    :align="align"
-                    :embed="embed"
-                    :x="x"
-                    :y="y"
-                    :right-click="rightClick" >
+        <!-- search bar -->
+        <div class="sm-input-area" v-if="!regular && query">
+            <input type="text" autocomplete="off" ref="input"
+                   v-model.trim="search"
+                   @keyup="processKey"
+                   @keydown.stop="processControl"
+                   class="sm-input">
+        </div>
 
-            <!-- header bar -->
-            <div class="sm-header" v-if="title || !regular || state.group">
-                <h3 v-text="headerText"></h3>
-                <button type="button" :title="i18n.select_all_btn" class="sm-selectall-button"
-                        @click="selectAll" v-if="!regular && multiple"><i class="sm-iconfont icon-selectall"></i></button>
-                <button type="button" :title="i18n.remove_all_btn" class="sm-removeall-button" @click="clear"
-                        v-if="!regular"><i class="sm-iconfont icon-removeall"></i></button>
-                <button type="button" @click="close" :title="i18n.close_btn" v-if="!embed"
-                        class="sm-close-button">×</button>
-            </div>
+        <!-- tabs bar -->
+        <div class="sm-result-tabs" v-if="state.group">
+            <ul>
+                <li v-for="(tab,index) in data" :key="index">
+                    <a href="javascript:void(0);"
+                       :tab-id="'selectmenu-tab-' + (index+1)"
+                       :data-index="index"
+                       @click="tabIndex = index"
+                       :class="{active:index === tabIndex}">{{tab.title}}</a>
+                </li>
+            </ul>
+        </div>
 
-            <!-- search bar -->
-            <div class="sm-input-area" v-if="!regular && query">
-                <input type="text" autocomplete="off" ref="input"
-                       v-model.trim="search"
-                       @keyup="processKey"
-                       @keydown.stop="processControl"
-                       class="sm-input">
-            </div>
-
-            <!-- tabs bar -->
-            <div class="sm-result-tabs" v-if="state.group">
-                <ul>
-                    <li v-for="(tab,index) in data" :key="index">
-                        <a href="javascript:void(0);"
-                           :tab-id="'selectmenu-tab-' + (index+1)"
-                           :data-index="index"
-                           @click="tabIndex = index"
-                           :class="{active:index === tabIndex}">{{tab.title}}</a>
-                    </li>
-                </ul>
-            </div>
-
-            <!-- results list -->
-
-            <v-regular-menu v-if="regular" :show="show" :data="results"
-                            :parent-instance="$parent" @close="close"></v-regular-menu>
-        </v-dropdown>
-    </div>
+        <!-- results list -->
+        <advanced v-if="!regular" :list="results" :search="search" :message="message" :selected="selected"></advanced>
+        <regular v-if="regular" :show="show" :data="results"
+                        :parent-instance="$parent" @close="close"></regular>
+    </v-dropdown>
 </template>
 
 <script>
@@ -71,20 +69,22 @@
     import props from './mixins/props';
     import methods from './mixins/methods';
 
-    //import drop from 'v-dropdown';
-    import drop from '../v-dropdown/Dropdown';
+    import drop from 'v-dropdown';
     import regular from './components/RegularMenu';
+    import advanced from './components/AdvancedMenu';
 
     export default {
         name: "v-selectmenu",
         components: {
-            'v-regular-menu': regular,
+            'regular': regular,
+            'advanced': advanced,
             'v-dropdown': drop
         },
         mixins: [props, methods],
         provide(){
             return {
-                parentInst: this.$parent
+                parentInst: this.$parent,
+                i18n: this.i18n
             };
         },
         data(){
@@ -108,7 +108,6 @@
                     top: '',
                     left: ''
                 },
-                listStyle: {},
                 state: {
                     group: false
                 }
@@ -129,8 +128,8 @@
             },
             selected(val){
                 if(this.message && this.maxSelected && val.length < this.maxSelected) this.message = '';
-                this.$emit('input', val.concat().map(value=>value[this.keyField]).join(','));
-                this.$emit('values', val.concat());
+                this.$emit('input', val.slice().map(value=>value[this.keyField]).join(','));
+                this.$emit('values', val.slice());
             }
         },
         mounted(){
@@ -145,11 +144,13 @@
 
             if(this.data.length){
                 if(this.state.group) this.tabIndex = 0;
-                else this.results = this.data.concat();
+                else this.results = this.data.slice();
             }
             if(this.regular) this.menuClass['sm-regular'] = true;
             else this.init();
 
+
+            console.log(this.results)
             this.$on('clear', this.clear);
         },
         destroyed(){
--- src/components/v-selectmenu/components/AdvancedMenu.vue
+++ src/components/v-selectmenu/components/AdvancedMenu.vue
@@ -6,8 +6,8 @@
             <!-- eslint-disable-next-line -->
             <li pkey="11" v-for="(item,index) in list" :key="index" v-if="!message"
                 @click="selectItem(item)"
-                @mouseenter="highlight = index"
-                @mouseleave="highlight = -1"
+                @mouseenter="highlight(index)"
+                @mouseleave="highlight(-1)"
                 :class="{'sm-selected': selected.includes(item), 'sm-over': index===highlight }">
                 <div class="sm-selected-icon"><i class="sm-iconfont icon-selected"></i></div>
                 <div class="sm-item-text" v-html="getRowText(item)"></div>
@@ -32,7 +32,16 @@
             list: Array,
             scroll: Boolean,
             group: Boolean,
-            search: String
+            search: String,
+            message: String,
+            selected: Array,
+			value: Number
+        },
+        inject: ['i18n'],
+        data(){
+        	return {
+				listStyle: {},
+            };
         },
         computed: {
             classes(){
@@ -47,7 +56,10 @@
             getRowText(row){
                 if(typeof this.showField === 'string') return row[this.showField];
                 else if(typeof this.showField === 'function') return this.showField(row);
-            }
+            },
+			highlight(index){
+				this.$emit('input', index);
+			}
         }
     }
 </script>
