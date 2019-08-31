b021a86f1d352e4c60682c7add8af8de747b69ee
TerryZ <terry5@foxmail.com>
2019-08-31 11:17:45 +0800

    update

--- tests/unit/v-selectmenu/advanced.spec.js
+++ tests/unit/v-selectmenu/advanced.spec.js
@@ -1,7 +1,7 @@
 import { mount } from '@vue/test-utils'
 import { expect } from 'chai'
-import nbaTeams from '@test/sample/nba-teams'
 import { advancedGroup } from '@test/sample/menu/advanced'
+import nbaTeams from '@test/sample/nba-teams'
 import sm from '@/components/v-selectmenu/SelectMenu'
 
 describe('v-selectmenu advanced mode', () => {
