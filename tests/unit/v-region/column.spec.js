0c8195974546c417c1d2d14b04f123cada4fc98a
TerryZ <terry5@foxmail.com>
2019-10-08 20:17:09 +0800

    Update column.spec.js

---
+++ tests/unit/v-region/column.spec.js
@@ -0,0 +1,8 @@
+import { mount } from '@vue/test-utils'
+import { expect } from 'chai'
+
+import Column from '@/components/v-region/components/ColumnGroup'
+
+describe('v-region Column 竖排多列表模式', function () {
+
+})
