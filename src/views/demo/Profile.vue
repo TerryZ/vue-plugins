09601393e8355e8e5342240d4a5ff4685059db6a
TerryZ <terry5@foxmail.com>
2019-10-25 18:51:14 +0800

    update demos

--- src/views/demo/Profile.vue
+++ src/views/demo/Profile.vue
@@ -1,32 +1,29 @@
 <template>
-    <div>
+  <div class="p-4">
+    <h4>Vuex content</h4>
+    <p v-text="user && user.name"></p>
 
-        <div class="box box-solid">
-            <h4>Vuex content</h4>
-            <p v-text="user && user.name"></p>
+    <h4>Props content</h4>
+    <p v-text="userName"></p>
 
-            <h4>Props content</h4>
-            <p v-text="userName"></p>
+    <h4>Other content</h4>
+    <p>
+        互联网科技与金融的结合变革了传统金融的经营思路，促使了金融行业的转型。如何利用科技进一步推动社会与经济的发展，成为了科技和金融领域需要共同思考的问题。11 日下午金融科技主论坛，不仅有蚂蚁金服发布在金融云、AI、风控、生物识别、开放平台等领域的最新进展以及技术突破，更有世界级别大咖：联合国粮农署副总干事 Mr. Daniel J. Gustafson 分享“科技如何消除贫困，推动农业发展”等。
+        <!--<h3 class="box-title"><strong>asdfasf</strong> 的用户信息</h3>-->
+    </p>
 
-            <h4>Other content</h4>
-            <p>
-                互联网科技与金融的结合变革了传统金融的经营思路，促使了金融行业的转型。如何利用科技进一步推动社会与经济的发展，成为了科技和金融领域需要共同思考的问题。11 日下午金融科技主论坛，不仅有蚂蚁金服发布在金融云、AI、风控、生物识别、开放平台等领域的最新进展以及技术突破，更有世界级别大咖：联合国粮农署副总干事 Mr. Daniel J. Gustafson 分享“科技如何消除贫困，推动农业发展”等。
-                <!--<h3 class="box-title"><strong>asdfasf</strong> 的用户信息</h3>-->
-            </p>
+    <p v-for="(item, index) in list" :key="index">
+      <input type="text" v-model="models[index]" class="form-control" :placeholder="item">
+    </p>
 
-            <p v-for="(item, index) in list" :key="index">
-                <input type="text" v-model="models[index]" class="form-control" :placeholder="item">
-            </p>
-
-            <p >
-                <!-- <fa-icon icon="check"></fa-icon> -->
-                <!-- <fa-icon icon="times"></fa-icon> -->
-                <button type="button" class="btn btn-primary" @click="close" id="btn-modal-close"> close</button>
-                <button type="button" class="btn btn-default" @click="save"> 保存</button>
-            </p>
-        </div>
-
-    </div>
+    <p >
+      <!-- <fa-icon icon="times"></fa-icon> -->
+      <button type="button" class="btn btn-primary" @click="close" id="btn-modal-close">
+        <fa-icon icon="check"></fa-icon> close
+      </button>
+      <button type="button" class="btn btn-default" @click="save"> 保存</button>
+    </p>
+  </div>
 </template>
 
 <script>
@@ -61,7 +58,7 @@ export default {
   },
   mounted () {
     // console.log(store.state.user)
-    // console.log(this.$store)
+    console.log(this)
     /*
     let that = this;
     $(this.$refs.upload).eUploadBaseInit({
@@ -75,9 +72,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-    .box {
-        padding: 10px;
-    }
-</style>
