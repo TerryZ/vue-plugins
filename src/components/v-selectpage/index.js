a38914d4ffbbe7b1d048e73f019a69e8350b67a0
TerryZ <terry5@foxmail.com>
2020-08-01 16:38:03 +0800

    update v-selectpage

--- src/components/v-selectpage/index.js
+++ src/components/v-selectpage/index.js
@@ -1,25 +1,25 @@
-import selectPage from './SelectPage'
+import SelectPage from './SelectPage'
 
-const Plugin = {
-  install (Vue, option = {}) {
-    if (Object.keys(option).length) {
-      const props = selectPage.mixins[0].props
-      if (typeof option.title === 'string') props.title.default = option.title
-      if (typeof option.language === 'string') props.language.default = option.language
-      if (typeof option.placeholder === 'string') props.placeholder.default = option.placeholder
-      if (typeof option.pageSize === 'number') props.pageSize.default = option.pageSize
-      if (typeof option.rtl === 'boolean') props.rtl.default = option.rtl
+SelectPage.install = (Vue, options = {}) => {
+  if (Object.keys(options).length) {
+    const { title, language, placeholder, pageSize, rtl, dataLoad } = options
+    const props = SelectPage.mixins[0].props
 
-      if (option.dataLoad && typeof option.dataLoad === 'function') {
-        selectPage.extends = {
-          methods: {
-            dataLoad: option.dataLoad
-          }
+    if (typeof title === 'string') props.title.default = title
+    if (typeof language === 'string') props.language.default = language
+    if (typeof placeholder === 'string') props.placeholder.default = placeholder
+    if (typeof pageSize === 'number') props.pageSize.default = pageSize
+    if (typeof rtl === 'boolean') props.rtl.default = rtl
+
+    if (dataLoad && typeof dataLoad === 'function') {
+      SelectPage.extends = {
+        methods: {
+          dataLoad: dataLoad
         }
       }
     }
-    Vue.component(selectPage.name, selectPage)
   }
+  Vue.component(SelectPage.name, SelectPage)
 }
-export { selectPage as SelectPage }
-export default Plugin
+
+export default SelectPage
